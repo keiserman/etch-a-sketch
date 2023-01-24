5d26d2e5e7b0776f8e23b8ab4d68ea12dcb438d5
Kevin <eisermanka@gmail.com>
2023-01-24 13:08:18 -0500

    Added clear button and made grid fixed size regardless of square size

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 let gridSize = 16;
 let gridArea = gridSize * gridSize;
-let squareSize = 30;
 let mouseDown = false;
 
 const gridContainer = document.querySelector(".grid-container");
 const colorPicker = document.getElementById("colorpicker");
+const clearButton = document.getElementById("clear");
 let brushColor = colorPicker.value;
 
-gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 0fr`;
+gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
 
 document.body.onmousedown = function() { 
     mouseDown = true;
@@ -19,30 +19,32 @@ document.body.onmouseup = function() {
 
 colorPicker.addEventListener("input", function() {
     brushColor = colorPicker.value;
-})
+});
 
-createGrid(squareSize);
+clearButton.addEventListener("click", function() {
+    gridContainer.replaceChildren();
+    createGrid();
+});
+
+createGrid();
 
 // Create the grid
-function createGrid(squareSize) {
+function createGrid() {
     for (let i = 0; i < gridArea; i++) {
         const gridItem = document.createElement('div');
         gridItem.className = "grid-item";
-        gridItem.style.width = `${squareSize}px`;
-        gridItem.style.height = `${squareSize}px`;
         gridItem.style.backgroundColor = "white";
         gridContainer.appendChild(gridItem);
     }
-}
 
-
-document.querySelectorAll(".grid-item").forEach(item => {
-    item.addEventListener("mouseover", () => {
-        if (mouseDown) {
+    document.querySelectorAll(".grid-item").forEach(item => {
+        item.addEventListener("mouseover", () => {
+            if (mouseDown) {
+                item.style.backgroundColor = brushColor;
+            }
+        });
+        item.addEventListener("click", () => {
             item.style.backgroundColor = brushColor;
-        }
+        })
     });
-    item.addEventListener("click", () => {
-        item.style.backgroundColor = brushColor;
-    })
-});
+}    
